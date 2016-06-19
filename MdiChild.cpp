d705895c253f407ac0a8b6cb211725dce43b2e60
YH Yang <yhuiyang@gmail.com>
2016-06-20 00:26:08 +0800

    Add QTabWidget.
    Now one QWebView in each tab.

--- MdiChild.cpp
+++ MdiChild.cpp
@@ -11,316 +11,68 @@
 
 
 // -----------------------------------------------------------------------------------------------------
-// MdiChildWebVeiw
+// WebView: tab inside every mdi child.
 // -----------------------------------------------------------------------------------------------------
-
-MdiChild::MdiChild(QWidget *parent) : QMainWindow(parent)
+TabWebView::TabWebView(QWidget *parent) : QMainWindow(parent)
 {
-    _view = new QWebView;
-    setupToolBarAndStatusBar();
-
-    /* connect signals from QWebView */
-    connect(_view, SIGNAL(iconChanged(void)), this, SLOT(onWebViewIconChanged(void)));
-    connect(_view, SIGNAL(linkClicked(QUrl)), this, SLOT(onWebViewLinkClicked(QUrl)));
-    connect(_view, SIGNAL(loadFinished(bool)), this, SLOT(onWebViewLoadFinished(bool)));
-    connect(_view, SIGNAL(loadProgress(int)), this, SLOT(onWebViewLoadProgress(int)));
-    connect(_view, SIGNAL(loadStarted(void)), this, SLOT(onWebViewLoadStarted(void)));
-    //connect(_view, SIGNAL(selectionChanged()), this, SLOT(onWebViewSelectionChanged()));
-    connect(_view, SIGNAL(statusBarMessage(QString)), this, SLOT(onWebViewStatusBarMessage(QString)));
-    connect(_view, SIGNAL(titleChanged(QString)), this, SLOT(onWebViewTitleChanged(QString)));
-    connect(_view, SIGNAL(urlChanged(QUrl)), this, SLOT(onWebViewUrlChanged(QUrl)));
-
-    /* connect signals from QWebPage */
-    QWebPage *page = _view->page();
-    connect(page, SIGNAL(applicationCacheQuotaExceeded(QWebSecurityOrigin*,quint64,quint64)), this, SLOT(onWebPageApplicationCacheQuotaExceeded(QWebSecurityOrigin*,quint64,quint64)));
-    //connect(page, SIGNAL(contentsChanged()), this, SLOT(onWebPageContentsChanged()));
-    connect(page, SIGNAL(databaseQuotaExceeded(QWebFrame*,QString)), this, SLOT(onWebPageDatabaseQuotaExceeded(QWebFrame*,QString)));
-    connect(page, SIGNAL(downloadRequested(QNetworkRequest)), this, SLOT(onWebPageDownloadRequested(QNetworkRequest)));
-    connect(page, SIGNAL(featurePermissionRequestCanceled(QWebFrame*,QWebPage::Feature)), this, SLOT(onWebPageFeaturePermissionRequestCanceled(QWebFrame*,QWebPage::Feature)));
-    connect(page, SIGNAL(featurePermissionRequested(QWebFrame*,QWebPage::Feature)), this, SLOT(onWebPageFeaturePermissionRequested(QWebFrame*,QWebPage::Feature)));
-    connect(page, SIGNAL(frameCreated(QWebFrame*)), this, SLOT(onWebPageFrameCreated(QWebFrame*)));
-    connect(page, SIGNAL(geometryChangeRequested(QRect)), this, SLOT(onWebPageGeometryChangeRequested(QRect)));
-    connect(page, SIGNAL(linkClicked(QUrl)), this, SLOT(onWebPageLinkClicked(QUrl)));
-    connect(page, SIGNAL(linkHovered(QString,QString,QString)), this, SLOT(onWebPageLinkHovered(QString,QString,QString)));
-    connect(page, SIGNAL(loadFinished(bool)), this, SLOT(onWebPageLoadFinished(bool)));
-    connect(page, SIGNAL(loadProgress(int)), this, SLOT(onWebPageLoadProgress(int)));
-    connect(page, SIGNAL(loadStarted()), this, SLOT(onWebPageLoadStarted()));
-    connect(page, SIGNAL(menuBarVisibilityChangeRequested(bool)), this, SLOT(onWebPageMenuBarVisibilityChangeRequested(bool)));
-    //connect(page, SIGNAL(microFocusChanged()), this, SLOT(onWebPageMicroFocusChanged()));
-    connect(page, SIGNAL(printRequested(QWebFrame*)), this, SLOT(onWebPagePrintRequested(QWebFrame*)));
-    //connect(page, SIGNAL(repaintRequested(QRect)), this, SLOT(onWebPageRepaintRequested(QRect)));
-    connect(page, SIGNAL(restoreFrameStateRequested(QWebFrame*)), this, SLOT(onWebPageFrameCreated(QWebFrame*)));
-    connect(page, SIGNAL(saveFrameStateRequested(QWebFrame*,QWebHistoryItem*)), this, SLOT(onWebPageSaveFrameStateRequested(QWebFrame*,QWebHistoryItem*)));
-    //connect(page, SIGNAL(scrollRequested(int,int,QRect)), this, SLOT(onWebPageScrollRequested(int,int,QRect)));
-    //connect(page, SIGNAL(selectionChanged()), this, SLOT(onWebPageSelectionChanged()));
-    connect(page, SIGNAL(statusBarMessage(QString)), this, SLOT(onWebPageStatusBarMessage(QString)));
-    connect(page, SIGNAL(statusBarVisibilityChangeRequested(bool)), this, SLOT(onWebPageStatusBarVisibilityChangeRequested(bool)));
-    connect(page, SIGNAL(toolBarVisibilityChangeRequested(bool)), this, SLOT(onWebPageToolBarVisibilityChangeRequested(bool)));
-    connect(page, SIGNAL(unsupportedContent(QNetworkReply*)), this, SLOT(onWebPageUnsupportedContent(QNetworkReply*)));
-    connect(page, SIGNAL(viewportChangeRequested()), this, SLOT(onWebPageViewportChangeRequested()));
-    connect(page, SIGNAL(windowCloseRequested()), this, SLOT(onWebPageWindowCloseRequested()));
-
-    /* load accountId from account combobox */
-    qlonglong accountId = getCurrentAccountIdFromComboBox();
-
-    /* CastlAgeNetworkAccessManager */
-    _netMgr = new CastleAgeNetworkAccessManager(accountId, this);
+    _webView = new QWebView(this);
+    _netMgr = new CastleAgeNetworkAccessManager(0, this);
+    QWebPage *page = _webView->page();
     page->setNetworkAccessManager(_netMgr);
 
-    connect(_netMgr, SIGNAL(ca_login_done(qlonglong,bool)), this, SLOT(onCastleAgeLoginDone(qlonglong,bool)));
+    setupActionToolBar();
+    populateToolBar();
 
-    /* setup QWebView and initial url */
-    _view->load(QUrl("https://web3.castleagegame.com/castle_ws/index.php"));
-    _view->show();
+    connect(_webView, SIGNAL(loadStarted()), this, SLOT(onWebViewLoadStarted()));
+    connect(_webView, SIGNAL(loadProgress(int)), this, SLOT(onWebViewLoadProgress(int)));
+    connect(_webView, SIGNAL(loadFinished(bool)), this, SLOT(onWebViewLoadFinished(bool)));
+    connect(_webView, SIGNAL(urlChanged(QUrl)), this, SLOT(onWebViewUrlChanged(QUrl)));
+    connect(page, SIGNAL(linkHovered(QString,QString,QString)), this, SLOT(onWebPageLinkHovered(QString,QString,QString)));
+    connect(_netMgr, SIGNAL(ca_login_done(qlonglong,bool)), this, SLOT(onCastleAgeLoginResult(qlonglong,bool)));
 
-    this->setCentralWidget(_view);
-    this->setMinimumWidth(800);
+    _webView->load(QUrl("https://web3.castleagegame.com/castle_ws/index.php"));
+    _webView->show();
 
-    setAttribute(Qt::WA_DeleteOnClose);
+    this->setCentralWidget(_webView);
 }
 
-void MdiChild::setupToolBarAndStatusBar()
+void TabWebView::setupActionToolBar()
 {
-    /* navigation tool bar */
-    QToolBar *navigationToolBar = this->addToolBar(tr("Navigation ToolBar"));
+    QToolBar *toolbarNavigation = addToolBar("Navigation");
+    toolbarNavigation->addAction(_webView->pageAction(QWebPage::Reload));
+    toolbarNavigation->addAction(_webView->pageAction(QWebPage::Stop));
+    toolbarNavigation->addWidget(_address = new QLineEdit("", toolbarNavigation));
 
-    // actions
-    navigationToolBar->addAction(_view->pageAction(QWebPage::Reload));
-    navigationToolBar->addAction(_view->pageAction(QWebPage::Stop));
+    addToolBarBreak();
 
-    // address line edit
-    _address = new QLineEdit(this);
-    _address->setSizePolicy(QSizePolicy::Expanding, _address->sizePolicy().verticalPolicy());
-    connect(_address, SIGNAL(returnPressed()), this, SLOT(onAddressLineReturnPressed()));
-    navigationToolBar->addWidget(_address);
+    QToolBar *toolbarAccount = addToolBar("Account");
+    toolbarAccount->addWidget(new QLabel("Account:", toolbarAccount));
+    toolbarAccount->addWidget(_comboBoxAccount = new QComboBox(toolbarAccount));
 
-    this->addToolBarBreak();
-
-    /* account tool bar */
-    QToolBar *accountToolBar = this->addToolBar(tr("Account ToolBar"));
-    accountToolBar->addWidget(new QLabel(tr("Account: "), this));
-    _accountComboBox = new QComboBox(this);
-    QSqlQueryModel *model = new QSqlQueryModel;
-    model->setQuery("SELECT tbl_a.id, ifnull(ign, email), email, ifnull(ign, 'Unknown') || ' - ' || email FROM accounts AS tbl_a LEFT JOIN igns AS tbl_i ON tbl_i.accountId = tbl_a.id ORDER BY timestamp");
-    _accountComboBox->setModel(model);
-    _accountComboBox->setModelColumn(3); // 0: accountId, 1: ign, 2: email, 3: ign & email
-    accountToolBar->addWidget(_accountComboBox);
-
-    connect(_accountComboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(onAccountComboBoxIndexChanged(int)));
-
-    /* status bar */
-    this->setStatusBar(new QStatusBar);
+    connect(_address, &QLineEdit::returnPressed, this, &TabWebView::onAddressReturnKeyPressed);
+    connect(_comboBoxAccount, SIGNAL(currentIndexChanged(int)), this, SLOT(onAccountIndexChanged(int)));
 }
 
-qlonglong MdiChild::getCurrentAccountIdFromComboBox()
+void TabWebView::populateToolBar()
 {
-    int row = _accountComboBox->currentIndex();
-    int col = 0; // column 0: accountId, 1: ign, 2: email, 3: ign & email
-    QModelIndex idx = _accountComboBox->model()->index(row, col);
-    return _accountComboBox->model()->data(idx).toLongLong();
+    _comboBoxAccount->clear();
+    _comboBoxAccount->addItem("------ Select Account ------", 0);
+
+    QSqlQuery sql;
+    if (sql.exec("SELECT a.id, ifnull(ign, 'Unknown') || ' - ' || email FROM accounts AS a LEFT JOIN igns AS i ON i.accountId = a.id ORDER BY timestamp")) {
+        while (sql.next())
+            _comboBoxAccount->addItem(sql.value(1).toString(), sql.value(0).toLongLong());
+    }
 }
 
-//
-// Slots
-//
-void MdiChild::onWebViewIconChanged()
-{
-    qDebug() << "QWebView iconChanged";
-}
-
-void MdiChild::onWebViewLinkClicked(const QUrl &url)
-{
-    qDebug() << "QWebView linkClicked:" << url;
-}
-
-void MdiChild::onWebViewLoadFinished(bool ok)
-{
-    qDebug() << "QWebView loadFinished. ok:" << ok;
-}
-
-void MdiChild::onWebViewLoadProgress(int progress)
-{
-    qDebug() << "QWebView loadProgress:" << progress;
-}
-
-void MdiChild::onWebViewLoadStarted()
-{
-    qDebug() << "QWebView loadStarted";
-}
-
-void MdiChild::onWebViewSelectionChanged()
-{
-    qDebug() << "QWebView selectionChanged";
-}
-
-void MdiChild::onWebViewStatusBarMessage(const QString &text)
-{
-    qDebug() << "QWebView statusBarMessage:" << text;
-}
-
-void MdiChild::onWebViewTitleChanged(const QString &title)
-{
-    qDebug() << "QWebView titleChanged:" << title;
-}
-
-void MdiChild::onWebViewUrlChanged(const QUrl &url)
-{
-    qDebug() << "QWebView urlChanged:" << url;
-
-    _address->setText(url.toString());
-}
-
-void MdiChild::onWebPageApplicationCacheQuotaExceeded(QWebSecurityOrigin * origin, quint64 defaultOriginQuota, quint64 totalSpaceNeeded)
-{
-    qDebug() << "QWebPage applicationCacheQuotaExceeded" << origin << defaultOriginQuota << totalSpaceNeeded;
-}
-
-void MdiChild::onWebPageContentsChanged()
-{
-    qDebug() << "QWebPage contentsChanged";
-}
-
-void MdiChild::onWebPageDatabaseQuotaExceeded(QWebFrame * frame, QString databaseName)
-{
-    qDebug() << "QWebPage databaseQuotaExceeded" << frame << databaseName;
-}
-
-void MdiChild::onWebPageDownloadRequested(const QNetworkRequest & request)
-{
-    qDebug() << "QWebPage downloadRequested" << request.url();
-}
-
-void MdiChild::onWebPageFeaturePermissionRequestCanceled(QWebFrame * frame, QWebPage::Feature feature)
-{
-    qDebug() << "QWebPage featurePermissionRequestCanceled" << frame << feature;
-}
-
-void MdiChild::onWebPageFeaturePermissionRequested(QWebFrame * frame, QWebPage::Feature feature)
-{
-    qDebug() << "QWebPage featurePermissionRequested" << frame << feature;
-}
-
-void MdiChild::onWebPageFrameCreated(QWebFrame * frame)
-{
-    qDebug() << "QWebPage frameCreated" << frame;
-}
-
-void MdiChild::onWebPageGeometryChangeRequested(const QRect & geometry)
-{
-    qDebug() << "QWebPage geometryChangeRequested" << geometry;
-}
-
-void MdiChild::onWebPageLinkClicked(const QUrl & url)
-{
-    qDebug() << "QWebPage linkClicked" << url;
-}
-
-void MdiChild::onWebPageLinkHovered(const QString & link, const QString & title, const QString & textContent)
-{
-    //qDebug() << "QWebPage linkHovered" << link << title << textContent;
-    this->statusBar()->showMessage(link);
-}
-
-void MdiChild::onWebPageLoadFinished(bool ok)
-{
-    qDebug() << "QWebPage loadFinished" << ok;
-}
-
-void MdiChild::onWebPageLoadProgress(int progress)
-{
-    qDebug() << "QWebPage loadProgress" << progress;
-}
-
-void MdiChild::onWebPageLoadStarted()
-{
-    qDebug() << "QWebPage loadStarted";
-}
-
-void MdiChild::onWebPageMenuBarVisibilityChangeRequested(bool visible)
-{
-    qDebug() << "QWebPage menuBarVisibilityChangeRequested" << visible;
-}
-
-void MdiChild::onWebPageMicroFocusChanged()
-{
-    qDebug() << "QWebPage microFocusChanged";
-}
-
-void MdiChild::onWebPagePrintRequested(QWebFrame * frame)
-{
-    qDebug() << "QWebPage printRequested" << frame;
-}
-
-void MdiChild::onWebPageRepaintRequested(const QRect & dirtyRect)
-{
-    qDebug() << "QWebPage repaintRequested" << dirtyRect;
-}
-
-void MdiChild::onWebPageRestoreFrameStateRequested(QWebFrame * frame)
-{
-    qDebug() << "QWebPage restoreFrameStateRequested" << frame;
-}
-
-void MdiChild::onWebPageSaveFrameStateRequested(QWebFrame * frame, QWebHistoryItem * item)
-{
-    qDebug() << "QWebPage saveFrameStateRequested" << frame << item;
-}
-
-void MdiChild::onWebPageScrollRequested(int dx, int dy, const QRect & rectToScroll)
-{
-    qDebug() << "QWebPage scrollRequested" << dx << dy << rectToScroll;
-}
-
-void MdiChild::onWebPageSelectionChanged()
-{
-    qDebug() << "QWebPage selectionChanged";
-}
-
-void MdiChild::onWebPageStatusBarMessage(const QString & text)
-{
-    qDebug() << "QWebPage statusBarMessage" << text;
-}
-
-void MdiChild::onWebPageStatusBarVisibilityChangeRequested(bool visible)
-{
-    qDebug() << "QWebPage statusBarVisibilityChangeRequested" << visible;
-}
-
-void MdiChild::onWebPageToolBarVisibilityChangeRequested(bool visible)
-{
-    qDebug() << "QWebPage toolBarVisibilityChangeRequested" << visible;
-}
-
-void MdiChild::onWebPageUnsupportedContent(QNetworkReply * reply)
-{
-    qDebug() << "QWebPage unsupportedContent" << reply;
-}
-
-void MdiChild::onWebPageViewportChangeRequested()
-{
-    qDebug() << "QWebPage viewportChangeRequested";
-}
-
-void MdiChild::onWebPageWindowCloseRequested()
-{
-    qDebug() << "QWebPage windowCloseRequested";
-}
-
-void MdiChild::onAccountComboBoxIndexChanged(int row)
-{
-    int col = 0; // column 0: accountId, 1: ign, 2: email, 3: ign & email
-    QModelIndex idx = _accountComboBox->model()->index(row, col);
-    qlonglong accountId = _accountComboBox->model()->data(idx).toLongLong();
-    qDebug() << "AccountComboBox index changed" << row << accountId;
-    this->_netMgr->switchAccount(accountId);
-    this->_view->reload();
-}
-
-void MdiChild::onAddressLineReturnPressed()
+void TabWebView::onAddressReturnKeyPressed()
 {
     // accept only url to https://web3.castleagegame.com/castle_ws/
     bool addressUpdated = false;
     QString addressString = _address->text();
+    addressString = addressString.trimmed();
+    if (QString::compare(addressString, _address->text()))
+        addressUpdated = true;
     if (addressString.startsWith("http://")) {
         addressString.replace("http://", "https://");
         addressUpdated = true;
@@ -329,26 +81,111 @@ void MdiChild::onAddressLineReturnPressed()
         addressString.replace("https://web4.", "https://web3.");
         addressUpdated = true;
     }
+    if (addressUpdated)
+        _address->setText(addressString);
     if (addressString.startsWith("https://web3.castleagegame.com/castle_ws/")) {
-        if (addressUpdated)
-            _address->setText(addressString);
-        _view->load(QUrl(addressString));
+        _webView->load(QUrl(addressString));
     } else {
-        qWarning() << "Url is not in white list. Ignore!";
+        QMessageBox::critical(this, "Wrong URL address!", "Only url addresses start with 'http://web3.castleagegame.com/castle_ws/' are allowed.");
     }
 }
 
-void MdiChild::onCastleAgeLoginDone(qlonglong accountId, bool successful)
+void TabWebView::onAccountIndexChanged(int index)
 {
-    Q_UNUSED(accountId);
-    if (successful && accountId == getCurrentAccountIdFromComboBox()) {
-        QString addressString = _address->text();
-        if (!addressString.isEmpty()) {
-            if (addressString.startsWith("https://web3.castleagegame.com/castle_ws/")) {
-                _view->load(QUrl(addressString));
-            }
+    qlonglong accountId = _comboBoxAccount->itemData(index).toLongLong();
+    this->_netMgr->switchAccount(accountId);
+    this->_webView->reload();
+
+    emit requestUpdateTabInfo(this, accountId);
+}
+
+void TabWebView::onWebViewLoadStarted()
+{
+
+}
+
+void TabWebView::onWebViewLoadProgress(int progress)
+{
+
+}
+
+void TabWebView::onWebViewLoadFinished(bool ok)
+{
+
+}
+
+void TabWebView::onWebViewUrlChanged(const QUrl &url)
+{
+    if (_address != nullptr)
+        _address->setText(url.toString());
+}
+
+void TabWebView::onWebPageLinkHovered(const QString &link, const QString &title, const QString &textContent)
+{
+    Q_UNUSED(title);
+    Q_UNUSED(textContent);
+    emit requestShowStatusBarMessage(link);
+}
+
+void TabWebView::onCastleAgeLoginResult(qlonglong accountId, bool successful)
+{
+
+}
+
+// -----------------------------------------------------------------------------------------------------
+// MdiChild: mdi child or main window.
+// -----------------------------------------------------------------------------------------------------
+
+MdiChild::MdiChild(QWidget *parent) : QMainWindow(parent)
+{
+    setStatusBar(new QStatusBar);
+
+    QPushButton *btnAddTab = new QPushButton("+");
+    _tabWidget = new QTabWidget(this);
+    _tabWidget->setDocumentMode(true);
+    _tabWidget->setTabsClosable(true);
+    _tabWidget->setCornerWidget(btnAddTab);
+    this->setCentralWidget(_tabWidget);
+    this->setMinimumWidth(800);
+
+    connect(btnAddTab, &QPushButton::clicked, this, &MdiChild::addTabWebView);
+
+    addTabWebView();
+
+    setAttribute(Qt::WA_DeleteOnClose);
+}
+
+int MdiChild::addTabWebView()
+{
+    QString tabText = "NO ACCOUNT SELECTED";
+    TabWebView *tabWebView = new TabWebView(this);
+    int tabIndex = _tabWidget->addTab(tabWebView, tabText);
+
+    connect(tabWebView, &TabWebView::requestUpdateTabInfo, this, &MdiChild::onTabRequestUpdateTabInfo);
+    connect(tabWebView, &TabWebView::requestShowStatusBarMessage, this, &MdiChild::onTabRequestShowStatusBarMessage);
+
+    return tabIndex;
+}
+
+//
+// Slots
+//
+void MdiChild::onTabRequestUpdateTabInfo(TabWebView *const tabPage, qlonglong accountId)
+{
+    int tabIndex = _tabWidget->indexOf(tabPage);
+
+    if (tabIndex != -1) {
+        QSqlQuery sql;
+        sql.prepare("SELECT email, ifnull(ign, 'Unknown IGN') FROM accounts LEFT JOIN igns ON accounts.id = igns.accountId WHERE accounts.id = :accountId");
+        sql.bindValue(":accountId", accountId);
+        if (sql.exec() && sql.next()) {
+            _tabWidget->setTabText(tabIndex, sql.value(1).toString());
+            _tabWidget->setTabToolTip(tabIndex, sql.value(0).toString());
         }
-    } else {
-        qWarning() << "ca login failed";
     }
+}
+
+void MdiChild::onTabRequestShowStatusBarMessage(const QString &message)
+{
+    statusBar()->showMessage(message);
 }
--- MdiChild.h
+++ MdiChild.h
@@ -9,64 +9,54 @@ class QLineEdit;
 class QComboBox;
 class CastleAgeNetworkAccessManager;
 
+class TabWebView : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit TabWebView(QWidget *parent = 0);
+
+private:
+    void setupActionToolBar();
+    void populateToolBar();
+
+signals:
+    void requestShowStatusBarMessage(const QString &message);
+    void requestUpdateTabInfo(TabWebView * const self, qlonglong accountId);
+
+public slots:
+    void onAddressReturnKeyPressed();
+    void onAccountIndexChanged(int index);
+    void onWebViewLoadStarted();
+    void onWebViewLoadProgress(int progress);
+    void onWebViewLoadFinished(bool ok);
+    void onWebViewUrlChanged(const QUrl &url);
+    void onWebPageLinkHovered(const QString &link, const QString &title, const QString &textContent);
+    void onCastleAgeLoginResult(qlonglong accountId, bool successful);
+
+private:
+    QLineEdit *_address;
+    QWebView *_webView;
+    CastleAgeNetworkAccessManager *_netMgr;
+    QComboBox *_comboBoxAccount;
+};
+
 class MdiChild : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit MdiChild(QWidget *parent = 0);
+    int addTabWebView();
 
 signals:
 
 public slots:
-    void onWebViewIconChanged();
-    void onWebViewLinkClicked(const QUrl &url);
-    void onWebViewLoadFinished(bool ok);
-    void onWebViewLoadProgress(int progress);
-    void onWebViewLoadStarted();
-    void onWebViewSelectionChanged();
-    void onWebViewStatusBarMessage(const QString &text);
-    void onWebViewTitleChanged(const QString &title);
-    void onWebViewUrlChanged(const QUrl &url);
-    void onWebPageApplicationCacheQuotaExceeded(QWebSecurityOrigin * origin, quint64 defaultOriginQuota, quint64 totalSpaceNeeded);
-    void onWebPageContentsChanged();
-    void onWebPageDatabaseQuotaExceeded(QWebFrame * frame, QString databaseName);
-    void onWebPageDownloadRequested(const QNetworkRequest & request);
-    void onWebPageFeaturePermissionRequestCanceled(QWebFrame * frame, QWebPage::Feature feature);
-    void onWebPageFeaturePermissionRequested(QWebFrame * frame, QWebPage::Feature feature);
-    void onWebPageFrameCreated(QWebFrame * frame);
-    void onWebPageGeometryChangeRequested(const QRect & geom);
-    void onWebPageLinkClicked(const QUrl & url);
-    void onWebPageLinkHovered(const QString & link, const QString & title, const QString & textContent);
-    void onWebPageLoadFinished(bool ok);
-    void onWebPageLoadProgress(int progress);
-    void onWebPageLoadStarted();
-    void onWebPageMenuBarVisibilityChangeRequested(bool visible);
-    void onWebPageMicroFocusChanged();
-    void onWebPagePrintRequested(QWebFrame * frame);
-    void onWebPageRepaintRequested(const QRect & dirtyRect);
-    void onWebPageRestoreFrameStateRequested(QWebFrame * frame);
-    void onWebPageSaveFrameStateRequested(QWebFrame * frame, QWebHistoryItem * item);
-    void onWebPageScrollRequested(int dx, int dy, const QRect & rectToScroll);
-    void onWebPageSelectionChanged();
-    void onWebPageStatusBarMessage(const QString & text);
-    void onWebPageStatusBarVisibilityChangeRequested(bool visible);
-    void onWebPageToolBarVisibilityChangeRequested(bool visible);
-    void onWebPageUnsupportedContent(QNetworkReply * reply);
-    void onWebPageViewportChangeRequested();
-    void onWebPageWindowCloseRequested();
-    void onAccountComboBoxIndexChanged(int row);
-    void onAddressLineReturnPressed();
-    void onCastleAgeLoginDone(qlonglong accountId, bool successful);
+    void onTabRequestUpdateTabInfo(TabWebView * const tabPage, qlonglong accountId);
+    void onTabRequestShowStatusBarMessage(const QString &message);
 
 private:
-    void setupToolBarAndStatusBar();
-    qlonglong getCurrentAccountIdFromComboBox();
 
 private:
-    QWebView *_view;
-    QLineEdit *_address;
-    CastleAgeNetworkAccessManager *_netMgr;
-    QComboBox *_accountComboBox;
+    QTabWidget *_tabWidget;
 };
 
 #endif // MDICHILD_H
