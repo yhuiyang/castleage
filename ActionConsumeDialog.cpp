71e0740f8a38df28f89a9c6126333112b90d5d4b
YH Yang <yhuiyang@gmail.com>
2016-07-15 17:52:22 +0800

    Add action consume function.

--- ActionConsumeDialog.cpp
+++ ActionConsumeDialog.cpp
@@ -24,6 +24,10 @@ ActionConsumeDialog::ActionConsumeDialog(QWidget *parent) :
     connect(ui->comboBoxDefenderGuild, SIGNAL(currentIndexChanged(int)), this, SLOT(populateDefendGuildOfficersAndMembers(int)));
     populateDefendGuild();
     connect(ui->pushButtonRefreshLand, &QPushButton::clicked, this, &ActionConsumeDialog::onRefreshLandDefenders);
+
+    /* controls */
+    connect(ui->pushButtonStart, &QPushButton::pressed, this, &ActionConsumeDialog::onStartActionConsume);
+    connect(ui->pushButtonStop, &QPushButton::clicked, this, &ActionConsumeDialog::onStopActionConsume);
 }
 
 ActionConsumeDialog::~ActionConsumeDialog()
@@ -40,9 +44,10 @@ void ActionConsumeDialog::onAttackerTagOrGuildItemChanged(QListWidgetItem *item)
     QVariant checkState = item->data(Qt::CheckStateRole);
     if (checkState == Qt::Checked) {
         _checkedAttackerTagIdOrGuildId.append(tagIdOrGuildId);
-    } else
+    } else {
         if (!_checkedAttackerTagIdOrGuildId.removeOne(tagIdOrGuildId))
             qWarning() << "remove tag id or guild id that didn't exist.";
+    }
 
     /* reset attacker table */
     ui->tableWidgetAttackers->setColumnCount(4);
@@ -65,10 +70,10 @@ void ActionConsumeDialog::onAttackerTagOrGuildItemChanged(QListWidgetItem *item)
             sql.bindValue(":guildId", idAsString);
         } else { // tag id
             sql.prepare("SELECT a.id, ifnull(i.ign, 'UnknownIGN'), a.email FROM accounts AS a "
-                        "LEFT JOIN account_tag_mapping AS m ON m.accountId = a.id AND m.tagId = :tagId "
+                        "LEFT JOIN igns AS i ON i.accountId = a.id "
+                        "INNER JOIN account_tag_mapping AS m ON m.accountId = a.id AND m.tagId = :tagId "
                         "ORDER BY a.id");
             sql.bindValue(":tagId", id.toInt());
-
         }
 
         if (sql.exec()) {
@@ -236,6 +241,133 @@ void ActionConsumeDialog::onRefreshLandDefenders(bool checked)
     return;
 }
 
+void ActionConsumeDialog::onStartActionConsume()
+{
+    disableMofifyConfig(true);
+
+    /* attackers */
+    QList<QPair<QByteArray, qlonglong>> attackers;
+    for (int r = 0; r < ui->tableWidgetAttackers->rowCount(); r++) {
+        QTableWidgetItem *attacker_email_cell = ui->tableWidgetAttackers->item(r, 1);
+        attackers.append(QPair<QByteArray, qlonglong>(attacker_email_cell->data(Qt::DisplayRole).toByteArray(), attacker_email_cell->data(Qt::UserRole).toLongLong()));
+    }
+
+    /* target defenders */
+    QList<QString> targets;
+    for (int r = 0; r < ui->listWidgetDefenders->count(); r++) {
+        QListWidgetItem *defender = ui->listWidgetDefenders->item(r);
+        if (defender->data(Qt::CheckStateRole) == Qt::Checked)
+            targets.append(defender->data(Qt::UserRole).toString());
+    }
+
+    if (attackers.count() == 0 || targets.count() == 0) {
+        disableMofifyConfig(false);
+        if (attackers.count() == 0)
+            showLog("You didn't select any attackers.");
+        if (targets.count() == 0)
+            showLog("You didn't select any target defenders.");
+        return;
+    }
+
+    /* options */
+    QString defenderGuildId = ui->comboBoxDefenderGuild->currentData().toString();
+    bool bUseAllToken = ui->radioButtonTokenUseAll->isChecked();
+    bool bUseAtMostToken = ui->radioButtonTokenUseAtMost->isChecked();
+    bool bUseTokenUntil = ui->radioButtonTokenUseUntil->isChecked();
+    int useAtMostToken = bUseAtMostToken ? ui->spinBoxUseAtMostTokens->value() : -1;
+    int useTokenUntil = bUseTokenUntil ? ui->spinBoxUseUntil->value() : -1;
+    int stopActionLeft = ui->spinBoxStopActionLeft->value();
+
+    /* hit */
+    QSqlQuery sql;
+    QString target;
+    QString attackerIgn;
+    QWebFrame *frame = _page.mainFrame();
+    QList<QPair<QString,QString>> empty;
+    QList<QPair<QString,QString>> hitForm;
+    QList<QPair<QString,QString>> qs;
+    qs.append(QPair<QString,QString>("guild_id", ui->comboBoxDefenderGuild->currentData().toString()));
+    qs.append(QPair<QString,QString>("slot", QString::number(ui->spinBoxLandId->value())));
+
+    qsrand((uint)QTime::currentTime().msec());
+
+    SynchronizedNetworkAccessManager *attackerNAM;
+    QByteArray response;
+    sql.prepare("SELECT ifnull(i.ign, a.email) FROM accounts AS a LEFT JOIN igns AS i ON i.accountId = a.id WHERE a.id = :accountId");
+    for (QPair<QByteArray, qlonglong> attacker : attackers) {
+        sql.bindValue(":accountId", attacker.second);
+        if (sql.exec() && sql.next())
+            attackerIgn = "[" + sql.value(0).toString() + "]";
+        else
+            attackerIgn = "[XXX]";
+        attackerNAM = getNetworkAccessManager(attacker.second, attacker.first);
+
+        /* check land */
+        response = attackerNAM->ca_post("guildv2_conquest_expansion.php", empty, qs);
+        if (response.isEmpty()) {
+            showLog(attackerIgn + ": can not check land state. Make sure land is in vulnerable state now!");
+            continue;
+        }
+
+        int actionLeft;
+        int tickLeft;
+        QString timeLeft;
+        int tokenLeft, initialToken;
+        QList<QList<QPair<QString,QString>>> hitParams;
+
+        frame->setHtml(response);
+
+        /* action left & time left */
+        parseActionAndTimeLeftFromWebFrame(attackerIgn, frame, actionLeft, tickLeft, timeLeft);
+        if (actionLeft <= 0) {
+            showLog(attackerIgn + ": no action left, ignore.");
+            continue;
+        }
+        /* attacker token left */
+        parseAttackerTokenLeft(attackerIgn, frame, tokenLeft);
+        initialToken = tokenLeft;
+        /* hit defender params: note, if attacker has multiple attack methods, the params count will be increased. */
+        hitParams = parseAttackerHitParams(attackerIgn, frame);
+
+        target = targets.at(qrand() % targets.count());
+        hitForm = findHitParam(hitParams, target);
+        if (hitForm.size() == 0) {
+            showLog(attackerIgn + ": can not find param to hit specific target. Ignore.");
+            continue;
+        }
+
+        showLog(attackerIgn + ": time left: " + timeLeft + ", action left: " + QString::number(actionLeft) + ", token left: " + QString::number(tokenLeft) + ", next target: " + target);
+        while (
+               ((bUseAllToken || bUseAtMostToken) ? (tokenLeft > 0) : (tokenLeft > useTokenUntil))
+               && ((bUseAllToken || bUseTokenUntil) ? (initialToken - tokenLeft) < 20 : (initialToken - tokenLeft) < useAtMostToken)
+               && actionLeft > stopActionLeft
+               && hitForm.size() > 0
+               //&& false
+            )
+        {
+            response = attackerNAM->ca_post("guildv2_conquest_expansion.php", hitForm, empty);
+            if (response.isEmpty()) {
+                showLog(attackerIgn + ": can not check response after hit.");
+                break;
+            }
+
+            frame->setHtml(response);
+            parseActionAndTimeLeftFromWebFrame(attackerIgn, frame, actionLeft, tickLeft, timeLeft);
+            parseAttackerTokenLeft(attackerIgn, frame, tokenLeft);
+            target = targets.at(qrand() % targets.count());
+            hitForm = findHitParam(hitParams, target);
+            showLog(attackerIgn + ": time left: " + timeLeft + ", action left: " + QString::number(actionLeft) + ", token left: " + QString::number(tokenLeft) + ", next target: " + target);
+        }
+    }
+
+    disableMofifyConfig(false);
+}
+
+void ActionConsumeDialog::onStopActionConsume()
+{
+    disableMofifyConfig(false);
+}
+
 //
 // Internal helper methods
 //
@@ -265,16 +397,7 @@ void ActionConsumeDialog::populateAttackTagOrGuild()
 {
     QSqlQuery sql;
 
-    /* guilds */
-    if (sql.exec("SELECT guildid, name FROM guildids INNER JOIN guilds ON guilds.id = guildids.guildid GROUP BY guildid")) {
-        while (sql.next()) {
-            QListWidgetItem *item = new QListWidgetItem(ui->listWidgetTagOrGuild);
-            item->setData(Qt::DisplayRole, "Guild: " + sql.value(1).toString());
-            item->setData(Qt::CheckStateRole, Qt::Unchecked);
-            item->setData(Qt::UserRole, sql.value(0).toString());
-            ui->listWidgetTagOrGuild->addItem(item);
-        }
-    }
+    ui->listWidgetTagOrGuild->clear();
     /* tags */
     if (sql.exec("SELECT id, name FROM tags")) {
         while (sql.next()) {
@@ -282,6 +405,16 @@ void ActionConsumeDialog::populateAttackTagOrGuild()
             item->setData(Qt::DisplayRole, "Tag: " + sql.value(1).toString());
             item->setData(Qt::CheckStateRole, Qt::Unchecked);
             item->setData(Qt::UserRole, sql.value(0).toInt());
+            ui->listWidgetTagOrGuild->addItem(item);
+        }
+    }
+    /* guilds */
+    if (sql.exec("SELECT guildid, name FROM guildids INNER JOIN guilds ON guilds.id = guildids.guildid GROUP BY guildid")) {
+        while (sql.next()) {
+            QListWidgetItem *item = new QListWidgetItem(ui->listWidgetTagOrGuild);
+            item->setData(Qt::DisplayRole, "Guild: " + sql.value(1).toString());
+            item->setData(Qt::CheckStateRole, Qt::Unchecked);
+            item->setData(Qt::UserRole, sql.value(0).toString());
             ui->listWidgetTagOrGuild->addItem(item);
         }
     }
@@ -340,4 +473,108 @@ void ActionConsumeDialog::populateDefendGuildOfficersAndMembers(int selectedGuil
         while (sql.next())
             ui->comboBoxDefenderGuildMember->addItem(sql.value(1).toString(), sql.value(0).toLongLong());
     }
+}
+
+void ActionConsumeDialog::disableMofifyConfig(bool disable) {
+    ui->listWidgetTagOrGuild->setEnabled(!disable);
+    ui->listWidgetDefenders->setEnabled(!disable);
+    ui->groupBoxOptions->setEnabled(!disable);
+    ui->groupBoxDefender->setEnabled(!disable);
+
+    ui->pushButtonStart->setEnabled(!disable);
+    ui->pushButtonStop->setEnabled(disable);
+}
+
+QString ActionConsumeDialog::tickToTime(const QString &tick)
+{
+    int tickInt = tick.toInt();
+    int s = tickInt % 60;
+    int m = (tickInt / 60) % 60;
+    int h = tickInt / 3600;
+    return QString().sprintf("%02d:%02d:%02d", h, m, s);
+}
+
+void ActionConsumeDialog::parseActionAndTimeLeftFromWebFrame(const QString &ign, QWebFrame *htmlFrame, int &actionLeft, int &tickLeft, QString &timeLeft)
+{
+    QWebElement timeLeftElement = htmlFrame->findFirstElement("div > div > input#monsterTickerSecs");
+    if (!timeLeftElement.isNull()) {
+        QString tickString = timeLeftElement.attribute("value");
+        tickLeft = tickString.toInt();
+        timeLeft = tickToTime(tickString);
+        QWebElement actionLeftElement = timeLeftElement.parent().parent().nextSibling().findFirst("span");
+        if (!actionLeftElement.isNull()) {
+            QString actionLeftString = actionLeftElement.toPlainText();
+            //qDebug() << actionLeftString;
+            actionLeft = actionLeftString.toInt(nullptr, 0);
+        } else {
+            actionLeft = 0;
+            showLog(ign + ": can not find action counter.");
+        }
+    } else {
+        showLog(ign + ": can not find time left and action counter.");
+        actionLeft = 0;
+        tickLeft = 0;
+        timeLeft = "00:00:00";
+    }
+}
+
+void ActionConsumeDialog::parseAttackerTokenLeft(const QString &ign, QWebFrame *htmlFrame, int &tokenLeft)
+{
+    QWebElement tokenLeftElement = htmlFrame->findFirstElement("div#persistHomeConquestPlateOpen > div:nth-child(2) > div:nth-child(1)");
+    if (!tokenLeftElement.isNull()) {
+        QString tokenLeftValue = tokenLeftElement.toPlainText();
+        bool version1 = tokenLeftValue.startsWith("Tokens:");
+        // There are two version of tokenLeftValue
+        // 1. "Tokens:\n8"
+        // 2. "\n\t\t\t\t\t\t\t\t\t\t\tTokens:\n\n\t\t\t\t\t\t\t\t\t\t\t9\t\t\t\t\t\t\t\t\t\t"
+        QStringList tokenLeftSplitResult = tokenLeftValue.split('\n');
+        QString tokenLeftString = version1
+                ? (tokenLeftSplitResult.length() > 1 ? tokenLeftSplitResult[1] : "")
+                : (tokenLeftSplitResult.length() > 3 ? tokenLeftSplitResult[3] : "");
+        //qDebug() << tokenLeftString;
+        tokenLeft = tokenLeftString.toInt();
+    } else {
+        showLog(ign + ": can not find hown many token left.");
+        tokenLeft = 0;
+    }
+}
+
+QList<QList<QPair<QString,QString>>> ActionConsumeDialog::parseAttackerHitParams(const QString &ign, QWebFrame *htmlFrame)
+{
+    Q_UNUSED(ign);
+    QList<QList<QPair<QString,QString>>> hitParams;
+    QList<QPair<QString,QString>> hitParam;
+
+    QWebElementCollection hitDefenderParams = htmlFrame->findAllElements("div#your_guild_member_list_1 > div form[onsubmit*=\"guildv2_conquest_expansion.php\"]");
+    for (QWebElement hitDefenderParam: hitDefenderParams) {
+        hitParam.clear();
+        QWebElementCollection params = hitDefenderParam.findAll("input[type=hidden]");
+        for (QWebElement param: params) {
+            hitParam.append(QPair<QString,QString>(param.attribute("name"), param.attribute("value")));
+        }
+        if (hitParam.size() > 0)
+            hitParams.append(hitParam);
+    }
+
+    return hitParams;
+}
+
+QList<QPair<QString,QString>> ActionConsumeDialog::findHitParam(QList<QList<QPair<QString, QString> > > hitParams, QString target_fbid)
+{
+    bool fbid_matched, attack_type_matched;
+    for (QList<QPair<QString,QString>> hitParam : hitParams) {
+        fbid_matched = false;
+        attack_type_matched = false;
+        for (QPair<QString,QString> param : hitParam) {
+            if (param.first == "attack_type" && param.second == "normal")
+                attack_type_matched = true;
+            else if (param.first == "target_id" && param.second == target_fbid)
+                fbid_matched = true;
+        }
+
+        if (fbid_matched && attack_type_matched)
+            return hitParam;
+    }
+
+    return QList<QPair<QString,QString>>();
 }
--- ActionConsumeDialog.h
+++ ActionConsumeDialog.h
@@ -23,12 +23,20 @@ public slots:
     void onAttackerTagOrGuildItemChanged(QListWidgetItem *item);
     void onRefreshLandDefenders(bool checked = false);
     void populateDefendGuildOfficersAndMembers(int selectedGuildIndex);
+    void onStartActionConsume();
+    void onStopActionConsume();
 
 private:
     void showLog(const QString &message);
     SynchronizedNetworkAccessManager *getNetworkAccessManager(const qlonglong accountId, const QByteArray &accountEmail);
     void populateAttackTagOrGuild();
     void populateDefendGuild();
+    void disableMofifyConfig(bool disable);
+    QString tickToTime(const QString &tick);
+    void parseActionAndTimeLeftFromWebFrame(const QString &ign, QWebFrame *htmlFrame, int &actionLeft, int &tickLeft, QString &timeLeft);
+    void parseAttackerTokenLeft(const QString &ign, QWebFrame *htmlFrame, int &tokenLeft);
+    QList<QList<QPair<QString,QString>>> parseAttackerHitParams(const QString &ign, QWebFrame *htmlFrame);
+    QList<QPair<QString,QString>> findHitParam(QList<QList<QPair<QString,QString>>> hitParams, QString target_fbid);
 
 private:
     Ui::ActionConsumeDialog *ui;
