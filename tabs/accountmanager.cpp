3763d9b07a041783552947d616b54a387caffe3a
YH Yang <yhuiyang@gmail.com>
2017-08-22 16:38:20 +0800

    Setup column header in sql query statement.

--- tabs/accountmanager.cpp
+++ tabs/accountmanager.cpp
@@ -196,20 +196,13 @@ AccountModel::AccountModel(QWidget *parent) :
     QSqlQueryModel(parent)
 {
     QString sql;
-    sql.append("SELECT a._id, a.email, i.ign, f.fbid, g.guildId, r.role FROM accounts AS a ");
+    sql.append("SELECT a._id AS 'Id', a.email AS 'Email address', i.ign AS 'In game name', f.fbid AS 'Facebook id', g.guildId AS 'Guide', r.role AS 'Role' FROM accounts AS a ");
     sql.append("LEFT JOIN igns AS i ON i.accountId = a._id ");
     sql.append("LEFT JOIN fbids AS f ON f.accountId = a._id ");
     sql.append("LEFT JOIN account_guild_mappings AS g ON g.accountId == a._id ");
     sql.append("LEFT JOIN roles AS r ON r.accountId = a._id ");
     sql.append("ORDER BY a.sequence");
     this->setQuery(sql);
-
-    this->setHeaderData(0, Qt::Horizontal, QString("Id"));
-    this->setHeaderData(1, Qt::Horizontal, QString("Email address"));
-    this->setHeaderData(2, Qt::Horizontal, QString("In game name"));
-    this->setHeaderData(3, Qt::Horizontal, QString("Facebook id"));
-    this->setHeaderData(4, Qt::Horizontal, QString("Guild"));
-    this->setHeaderData(5, Qt::Horizontal, QString("Role"));
 }
 
 AccountModel::~AccountModel()
